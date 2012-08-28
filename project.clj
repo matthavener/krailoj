b820483f974628aa324b9459f34a44cc8f34b60e
Matt H <matt@matt.xen.prgmr.com>
2012-08-28 15:05:03 +0000

    update irclj to fixed version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [
     [org.clojure/clojure "1.3.0"] 
     [org.clojure/data.csv "0.1.2"]
-    [irclj "0.5.0-SNAPSHOT"]
+    [irclj "0.5.0-alpha1"]
     [clj-http "0.3.3"]
     ]
   :main krailoj.core)
